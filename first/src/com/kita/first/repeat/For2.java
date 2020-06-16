d8d28bbda7b279d9ba569751262a6a925599462e
Administrator <Administrator@DESKTOP-ISVFPRU>
2020-06-16 10:47:12 +0900

    for2

---
+++ first/src/com/kita/first/repeat/For2.java
@@ -0,0 +1,17 @@
+package com.kita.first.repeat;
+
+public class For2 {
+	public static void main(String[] args) {
+
+		for (int i = 0; i < 3; i++) {
+
+			for (int z = 0; z < 5; z++) {
+
+				for (int r = 0; r < 3; r++) {
+					System.out.printf("%d - %d - %d\n", i, z, r);
+				}
+			}
+		}
+	}
+}
+//0-0-0 끝에서 부터 1씩 오르는 반복문
