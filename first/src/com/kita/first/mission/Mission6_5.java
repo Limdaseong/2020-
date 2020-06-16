6123eb37084e7a68ee85d0a84ac0967179ff4857
Administrator <Administrator@DESKTOP-ISVFPRU>
2020-06-16 17:10:17 +0900

    제곱 별찍기

--- first/src/com/kita/first/mission/Mission6_5.java
+++ first/src/com/kita/first/mission/Mission6_5.java
@@ -2,9 +2,12 @@ package com.kita.first.mission;
 
 public class Mission6_5 {
 	public static void main(String[] args) {
-		int star = 5;
-		for(int i=0;i<star;i++) {
-			
+		int star = 3;
+		for (int i = 0; i < star; i++) {
+			for (int z = 0; z < star; z++) {
+				System.out.print("*");
+			}
+			System.out.println();
 		}
 	}
 }
